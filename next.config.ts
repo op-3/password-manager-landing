e67530d7c05f2b2a128becb136d83d4b0629d48f
op-3 <19bxby@gmail.com>
2025-01-10 16:11:07 +0400

    update

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has type errors.
     ignoreBuildErrors: true,
   },
 };
